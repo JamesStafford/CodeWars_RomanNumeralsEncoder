be16fb8841f7f4b73396b9838378571a6564b2a5
jstafford <CatMouse2017!>
2022-03-27 17:08:53 -0500

    Encoding 1 to 24 with new method.

--- src/RomanNumeralsEncoder1.js
+++ src/RomanNumeralsEncoder1.js
@@ -1,18 +1,37 @@
+function addRomanNumeralSeries(number, romanNumerals) {
+    for (let i = 0; i < number; i++) {
+        if(i > 2) { // Cannot have a series characters with more than 3 identical characters
+            break;
+        }
+        romanNumerals.push("I");
+    }
+}
+
 export function encode1(number) {
     const romanNumerals = [];
     if (number < 4) {
-        for(let i = 0; i < number; i++){
-            romanNumerals.push("I");
-        }
+        addRomanNumeralSeries(number, romanNumerals);
     } else if (number === 4) {
         return "IV";
-    } else if (number === 5) {
-        return "V";
-    } else if (number > 5 && number < 10) {
+    } else if (number >= 5 && number < 9) {
         romanNumerals.push("V");
-        for (let i = 0; i < (number-5); i++) {
-            romanNumerals.push("I");
-        }
+        addRomanNumeralSeries(number-5, romanNumerals);
+    } else if (number === 9) {
+        return "IX";
+    } else if (number >= 10 && number < 14) {
+        romanNumerals.push("X");
+        addRomanNumeralSeries(number-10, romanNumerals);
+    } else if (number === 14) {
+        return "XIV";
+    } else if (number >= 15 && number < 19) {
+        romanNumerals.push("XV");
+        addRomanNumeralSeries(number-15, romanNumerals);
+    } else if (number === 19) {
+        romanNumerals.push("XIX");
+    } else if (number >= 20 && number < 24) {
+        romanNumerals.push("XX");
+        addRomanNumeralSeries(number-20, romanNumerals);
     }
     return romanNumerals.join("");
 }
+
